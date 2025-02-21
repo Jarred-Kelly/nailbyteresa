b635ea07696fce52a6c399f5c3d88fd3c71bb4ad
Jarred <jk.jarredkelly26@gmail.com>
2025-02-21 23:44:30 +0200

    Services Component change

--- components/Services.tsx
+++ components/Services.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import Link from "next/link";
-import { useState } from "react";
 import { HoverEffect } from "@/components/ui/card-hover-effect";
 import { PulsatingButton } from "@/components/magicui/pulsating-button";
 
